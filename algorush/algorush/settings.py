7df3796b0a710e083eecf6cf87a50e67cf99893e
SID-CJ <sidharthcj2004@gmail.com>
2024-07-23 20:04:43 +0530

    django setup

--- algorush/algorush/settings.py
+++ algorush/algorush/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.appsApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
