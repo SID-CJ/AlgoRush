20a2a949334b9cf16068939571c287e7ec6f3563
SID-CJ <sidharthcj2004@gmail.com>
2024-07-25 00:41:47 +0530

    basic structure

--- algorush/algorush/urls.py
+++ algorush/algorush/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api.urls')),
+    path('api/users',include('users.urls')),
+    path('api/buses',include('buses.urls'))
 ]
